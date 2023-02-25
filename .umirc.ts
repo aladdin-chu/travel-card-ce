71031b8ee9fa22d7a9f1b61059d3d531d8aa0c3a
Aladdin <zss.panda+0@gmail.com>
2023-02-25 22:51:24 +0800

    updare actions

--- .umirc.ts
+++ .umirc.ts
@@ -1,39 +1,41 @@
-import { defineConfig } from 'umi';
-const { ENV, PROXY } = process.env;
-const proxyUrl = '/proxy';
-const serverUrl = '';
-const publicPath = '//aladdin-chu.github.io/travel-card-ce/';
+import { defineConfig } from "umi";
+const { ENV, PROXY, REPO } = process.env;
+const proxyUrl = "/proxy";
+const serverUrl = "";
+const publicPath = REPO
+  ? "//entropycloud.gitee.io/travel-card-ce/"
+  : "//aladdin-chu.github.io/travel-card-ce/";
 
 export default defineConfig({
   nodeModulesTransform: {
-    type: 'none',
+    type: "none",
   },
   history: {
-    type: 'hash',
+    type: "hash",
   },
   hash: true,
   routes: [
-    { path: '/', component: '@/pages/index/index' },
-    { path: '/result', component: '@/pages/result/index' },
+    { path: "/", component: "@/pages/index/index" },
+    { path: "/result", component: "@/pages/result/index" },
   ],
   define: {
-    'process.env.mode': ENV ? 'development' : 'production',
-    'process.proxy': PROXY ? proxyUrl : null,
-    'process.admin_prefix': ENV ? '' : '',
-    'process.server_prefix': ENV ? proxyUrl : serverUrl,
+    "process.env.mode": ENV ? "development" : "production",
+    "process.proxy": PROXY ? proxyUrl : null,
+    "process.admin_prefix": ENV ? "" : "",
+    "process.server_prefix": ENV ? proxyUrl : serverUrl,
   },
   proxy: PROXY
     ? {
-      [`${proxyUrl}`]: {
-        target: PROXY,
-        changeOrigin: true,
-        pathRewrite: {
-          [`^${proxyUrl}`]: '',
+        [`${proxyUrl}`]: {
+          target: PROXY,
+          changeOrigin: true,
+          pathRewrite: {
+            [`^${proxyUrl}`]: "",
+          },
         },
-      },
-    }
+      }
     : {},
   fastRefresh: {},
-  publicPath: ENV ? '/' : publicPath,
-  title: '行程卡纪念版',
+  publicPath: ENV ? "/" : publicPath,
+  title: "行程卡纪念版",
 });
